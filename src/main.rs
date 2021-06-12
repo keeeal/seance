ee7f16fd29eeca76e5c319d8d07364a0b6742088
keeeal <james@keal.com.au>
2021-06-12 17:14:35 +0930

    simplify to not use groups

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 use bevy::prelude::*;
-use bevy_interact_2d::{Group, Interactable, InteractionPlugin, InteractionSource, InteractionState};
+use bevy_interact_2d::{Interactable, InteractionPlugin, InteractionSource, InteractionState};
 use rand::prelude::*;
 
-// Are groups really necessary?
-// What benefit do they provide over just defining and reusing components?
-const TRASH_GROUP: u8 = 0;
-const TRASHCAN_GROUP: u8 = 1;
-
-
-struct Clickable {
-    groups: Vec<Group>
-}
-
-
-impl Default for Clickable {
-    fn default() -> Self {
-        Self {
-            groups: vec![Group::default()],
-        }
-    }
-}
+struct Clickable;
 
 
 fn startup(
@@ -29,11 +12,7 @@ fn startup(
 ) {
     commands
         .spawn_bundle(OrthographicCameraBundle::new_2d())
-        .insert(InteractionSource {
-            groups: vec![Group(TRASHCAN_GROUP), Group(TRASH_GROUP)],
-            ..Default::default()
-    });
-
+        .insert(InteractionSource::default());
 
     // load textures from sprite sheets
     let trashcan_texture = asset_server.load("trashcan.png");
@@ -69,14 +48,10 @@ fn startup(
                 ..Default::default()
             })
             .insert(Interactable {
-                groups: vec![Group(TRASH_GROUP)],
                 bounding_box: (Vec2::new(-12., -12.), Vec2::new(12., 12.)),
                 ..Default::default()
             })
-            .insert(Clickable {
-                groups: vec![Group(TRASH_GROUP)],
-                ..Default::default()
-            })
+            .insert(Clickable)
             .id();
 
         entities.push(trash);
@@ -99,26 +74,16 @@ fn startup(
 }
 
 
-fn click_trash(
-    mut commands: Commands,
+fn click(
     mouse_button_input: Res<Input<MouseButton>>,
     interaction_state: Res<InteractionState>,
-    clickables: Query<(Entity, &Clickable), With<Clickable>>,
 ) {
     if !mouse_button_input.just_pressed(MouseButton::Left) {
         return
     }
 
-    // Is this the only / best way to determine if the entity was clicked?
-    for (entity, clickable) in clickables.iter() {
-        for group in clickable.groups.iter() {
-            if let Some(list) = interaction_state.ordered_interact_list_map.get(group) {
-                if let Some((_, position)) = list.iter().find(|(e, _)| e == &entity) {
-                    info!("trash clicked");
-                    break;
-                }
-            }
-        }
+    if !interaction_state.ordered_interact_list_map.is_empty() {
+        info!("entity clicked");
     }
 }
 
@@ -128,6 +93,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(InteractionPlugin)
         .add_startup_system(startup.system())
-        .add_system(click_trash.system())
+        .add_system(click.system())
         .run();
 }
